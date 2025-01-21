305e561ab51111016ab140c771e8aacf7c399fcb
rahul karakoti <rahulsinghkarakoti28@gmail.com>
2025-01-21 16:50:55 +0530

    added deployed link

--- src/api/axiosInstence.js
+++ src/api/axiosInstence.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const BASE_URL="https://social-media-backend-asgmt.onrender.com/api/v1"
-const BASE_URL="http://localhost:3000/api/v1"
+const BASE_URL="https://social-media-backend-v2-nizf.onrender.com/api/v1"
+// const BASE_URL="http://localhost:3000/api/v1"
 
 const axiosInstance = axios.create({
 
